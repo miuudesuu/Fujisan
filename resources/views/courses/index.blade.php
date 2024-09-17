ff9b5e571a6f391c525eab1421dd5d279177e832
miuudesuu <miyu.1jz_gte@icloud.com>
2024-09-17 18:22:02 +0900

    登山ルートのデザイン

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -15,13 +15,36 @@
         <body>
     <style>
         body{
-            margin:20px
+            margin:20px;
+            background-color: cornsilk;
+        }
+
+        .mokuji{
+            margin: auto;
+            text-align:center;
+            border:2px solid black;
+            background-color: beige;
+            padding:8px;
+            width:70%;
         }
     </style>
-            <h1>Blog Name</h1>
+            <h2 class="mokuji">
+                <strong>目次</strong><br>
+                
+                １．富士山のルートとは<br>
+                ２．<吉田ルート<br>
+                ３．須走ルート<br>
+                ４．御殿場ルート<br>
+                ５．富士宮ルート<br>
+            <br>
+            </h2>
             
-            <p>その1. 吉田ルート</p>
-            ログインユーザー：{{ Auth::user()->name }}
+            <p>その1. 富士山のルートとは</p>
+            <p>その2. 吉田ルート</p>
+            <p>その3. 須走ルート</p>
+            <p>その4. 御殿場ルート</p>
+            <p>その5. 富士宮ルート</p>
+            <footer>ログインユーザー：{{ Auth::user()->name}}</footer>
         </body>
     </x-app-layout>
 </html>
