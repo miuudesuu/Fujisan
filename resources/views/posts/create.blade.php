c0689fd77e82aed924d4bca3cabe0db24a9e9067
miuudesuu <miyu.1jz_gte@icloud.com>
2024-09-28 22:15:03 +0900

    投稿機能デザイン実装2

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -84,7 +84,6 @@
                       @endforeach
                   </select>
               </div><br>
-      
             <div class="sm:col-span-2">
               <label for="message" class="mb-2 inline-block text-sm text-gray-800 sm:text-base">投稿文</label>
               <textarea name="post[body]" class="h-64 w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"></textarea>
