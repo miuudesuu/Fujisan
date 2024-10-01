0e175aef23f111cf9a66e7c9eaba208503c28453
miuudesuu <miyu.1jz_gte@icloud.com>
2024-10-01 15:44:45 +0900

    全体的な実装

--- resources/views/answers/create.blade.php
+++ resources/views/answers/create.blade.php
@@ -13,20 +13,77 @@
         body{
             margin:20px
         }
+        .btn-square-slant {
+        text-align:center;
+        margin: 20px; /* ボタンの下にスペースを追加 */
+        display: inline-block;
+        position: relative;
+        padding: 0.5em 1.4em;
+        text-decoration: none;
+        background: #668ad8;/*ボタン色*/
+        color: #FFF;
+        border-bottom: solid 5px #36528c;/*ボタン色より暗めに*/
+        border-right: solid 5px #5375bd;/*ボタン色より暗めに*/
+      }
+      
+      .btn-square-slant:before {  
+        content: " ";
+        position: absolute;
+        bottom: -5px;
+        left: -1px;
+        width: 0;
+        height: 0;
+        border-width: 0 6px 6px 0px;
+        border-style: solid;
+        border-color: transparent;
+        border-bottom-color: #FFF;
+      }
+      .btn-square-slant:after {  
+        content: " ";
+        position: absolute;
+        top: -1px;
+        right: -5px;
+        width: 0;
+        height: 0;
+        border-width: 0px 6px 6px 0px;
+        border-style: solid;
+        border-color: #FFF;
+        border-bottom-color: transparent;
+      }
+      
+      .btn-square-slant:active {
+        /*ボタンを押したとき*/
+        border:none;
+        -webkit-transform: translate(6px,6px);
+        transform: translate(6px,6px);
+      }
     </style>
-        <h1>Blog Name</h1>
         <form action="/answers" method="POST">
-            @csrf
-            <div class="body">
-                <h2>Body</h2>
-                <textarea name="answer[body]" placeholder="それいいね！"></textarea>
-                <input type="hidden" name="answer[question_id]" value="{{$question->id}}">
-            </div>
-            <input type="submit" value="store">
-        </form>
-        <div class="footer">
-            <a href="/questions">戻る</a>
+        <div class="bg-white py-6 sm:py-8 lg:py-12">
+            <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+                <!-- text - start -->
+                <div class="mb-10 md:mb-16">
+                  <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">answer投稿</h2>
+            
+                  <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">投稿に対する、共感や感想などをコメントしよう！<br>投稿者がハッピーになるような、うれしくなるようなコメントをしましょう！
+                </div>
+                    @csrf
+            
+                  <div class="sm:col-span-2">
+                    <label for="message" class="mb-2 inline-block text-sm text-gray-800 sm:text-base">回答</label>
+                    <textarea name="answer[body]" placeholder="回答" class="h-64 w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"></textarea>
+                    <input type="hidden" name="answer[question_id]" value="{{$question->id}}">
+                  </div><br>
+            
+                  <div class="flex items-center justify-between sm:col-span-2">
+                    <button class="btn-square-slant"><input type="submit" value="投稿"></button>
+                    </form>
+                    <span class="text-sm text-gray-500"><a href="/questions">戻る</a></span>
+                  </div>
+                <!-- form - end -->
+              </div>
         </div>
+        </form>
     </body>
     </x-app-layout>
 </html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    
+    
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,6 +12,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
